61de9591702f9850f21bdc03a1ec85a89462811e
SawangS <poolohit@outlook.com>
2025-01-15 17:56:25 +0700

    feat: ...

--- routes/asset_routes/asset_routes.go
+++ routes/asset_routes/asset_routes.go
@@ -5,14 +5,13 @@ import (
 	// "citizen_system_back/middleware"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
 // RegisterAssetRoutes registers routes for asset-related operations
 // @Security CookieAuth
-func RegisterAssetRoutes(router *gin.RouterGroup, db *gorm.DB) {
-	router.GET("/assets", asset_controllers.ListAssets(db))
-	// router.GET("/assets", middleware.AuthMiddleware, asset_controllers.ListAssets(db))
-	router.GET("/assets/:id", asset_controllers.GetAssetByID(db))
-	router.GET("/assets/get_asset_by_user_id/:user_id", asset_controllers.GetAssetByUserID(db))
+func RegisterAssetRoutes(router *gin.RouterGroup) {
+	router.GET("/assets", asset_controllers.ListAssets())
+	// router.GET("/assets", middleware.AuthMiddleware, asset_controllers.ListAssets())
+	router.GET("/assets/:id", asset_controllers.GetAssetByID())
+	router.GET("/assets/get_asset_by_user_id/:user_id", asset_controllers.GetAssetByUserID())
 }
--- routes/routes.go
+++ routes/routes.go
@@ -2,8 +2,6 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
-	// "gorm.io/gorm"
 
 	"citizen_system_back/routes/asset_routes"
 	"citizen_system_back/routes/dev_tools_routes"
@@ -11,10 +9,10 @@ import (
 
 // RegisterRoutes registers all application routes.
 // @Security CookieAuth
-func RegisterRoutes(router *gin.Engine, db *gorm.DB) {
+func RegisterRoutes(router *gin.Engine) {
 	// Register Asset routes with the database passed as a dependency
 	assetGroup := router.Group("/api/v1")
 	// assetGroup.Use(middleware.AuthMiddleware)
-	asset_routes.RegisterAssetRoutes(assetGroup, db)
-	dev_tools_routes.RegisterDevToolsRoutes(assetGroup, db)
+	asset_routes.RegisterAssetRoutes(assetGroup)
+	dev_tools_routes.RegisterDevToolsRoutes(assetGroup)
 }
